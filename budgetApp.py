aa39e46c4f2ceb08f15051913409bea3336f4a17
Tysmithyyy <tylerelijahsmith15@gmail.com>
2023-03-02 10:44:41 -0700

    made connections to db tables, added user sign up

--- budgetApp.py
+++ budgetApp.py
@@ -5,26 +5,6 @@ import pandas as pd
 from pathlib import Path
 import pickle
 
-st.write(st.experimental_user['email'])
-# names = ["Tyler Smith", "Ava"]
-# usernames = ["tysmithyyy", "aavvaa"]
-
-# file_path = Path(__file__).parent / "hashed_pw.pkl"
-# with file_path.open("rb") as file:
-#     hashed_passwords = pickle.load(file)
-
-# authenticator = stauth.Authenticate(names, usernames, hashed_passwords, "budget_app_auth", "budget_app_auth")
-
-# name, authentication_status, username = authenticator.login("Login", "main")
-
-# if authentication_status:
-#     authenticator.logout('Logout', 'main')
-#     st.write(f'Welcome *{name}*')
-#     st.title('Some content')
-# elif authentication_status is False:
-#     st.error('Username/password is incorrect')
-# elif authentication_status is None:
-#     st.warning('Please enter your username and password')
 
 # Initialize connection.
 # Uses st.cache_resource to only run once.
@@ -41,14 +21,75 @@ def run_query(query):
     with conn.cursor() as cur:
         cur.execute(query)
         return cur.fetchall()
+    
+@st.cache_data(ttl=600)
+def run_command(command):
+    with conn.cursor() as cur:
+        cur.execute(command)
+        conn.commit()
 
-rows = run_query("SELECT * from userTable;")
+st.title("Smith Budgets")
+useremail = st.experimental_user['email']
 
-# Print results.
-for row in rows:
-    st.write(f"{row[0]} has a :{row[1]}:")
+users = run_query("SELECT * FROM public.userstable")
+user_email_list =  []
+for row in users:
+    user_email_list.append(row[0])
 
-# SIDEBAR
-# authenticator.logout("Logout", "sidebar")
-# st.sidebar.title(f"Welcom {name}")
-st.sidebar.header("This is the sidebar")
+if useremail not in user_email_list:
+    st.write("Looks like you're new here!")
+    st.write("What is your name?")
+    new_user_name = st.text_input("Name")
+    if st.button("Sign me up!"):
+        command = (f"INSERT INTO userstable (email, name) VALUES ('{useremail}', '{new_user_name}')")
+        run_command(command) 
+        users = run_query("SELECT * FROM userstable")
+        for row in users:
+            user_email_list.append(row[0])
+
+if useremail in user_email_list:
+    st.write(f"Welcome!")
+    budgets = run_query("SELECT * from budgettable;")
+
+    st.subheader("Which budget do you want to look at?")
+
+    col1, col2 = st.columns(2)
+
+    with col1:
+        budget_names = []
+        for row in budgets:
+            budget_names.append(row[0])
+        budget_name = st.selectbox("Budgets", budget_names)
+    with col2:
+        st.header("Or...")
+        st.subheader("Create New Budget")
+        new_budget_name = st.text_input("New Budget Name")
+        new_budget_allotment = st.number_input("New Budget Allotment")
+        if st.button("Save New Budget"):
+            run_command(f"INSERT INTO public.budgettable (budgetname, email, budgetallotment) VALUES ('{new_budget_name}', '{useremail}', '{new_budget_allotment}')")
+
+    # INSERT INTO films (code, title, did, date_prod, kind)
+    #     VALUES ('T_601', 'Yojimbo', 106, '1961-06-16', 'Drama');
+
+
+    st.subheader(budget_name)
+    tab1, tab2, tab3 = st.tabs(["View Budget", "Add Transaction", "Budget Settings"])
+
+    with tab1:
+        st.header("View Budget")
+        budget_df = pd.read_sql(f"SELECT * FROM transactionTable WHERE budgetName = '{budget_name}'", conn)
+
+    with tab2:
+        st.header("Add Transaction")
+        new_transaction_date = st.date_input("Transaction Date")
+        new_transaction_ammount = st.number_input("Transaction Ammount",step=0.1)
+        new_transaction_note = st.text_area("Note/Description", "input note here", 2)
+        st.button("Add Transaction")
+
+    with tab3:
+        st.header("Budget Settings")
+    # SIDEBAR
+    # authenticator.logout("Logout", "sidebar")
+    # st.sidebar.title(f"Welcom {name}")
+    # st.sidebar.header("This is the sidebar")
+    # st.sidebar.sub
